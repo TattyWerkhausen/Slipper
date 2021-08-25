deac6ee9fcf17a31943efda7ba6a78c38ef6c9bb
Tatty Werkhausen <68560755+TattyWerkhausen@users.noreply.github.com>
2021-08-25 15:46:19 -0300

    slipper parte 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FotosComponent } from './paginas/fotos/fotos.component';
+import { QuemSomosComponent } from './paginas/quem-somos/quem-somos.component';
+import { TemplateComponent } from './template/header/template.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'header', component: TemplateComponent},
+  {path: 'header/fotos', component: FotosComponent},
+  {path: 'fotos', component: FotosComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
